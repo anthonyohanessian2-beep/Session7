3f6fb4f3e9bf233083a75b4442ff5bb19842f972
anthonyohanessian <anthonyohanessian2@gmail.com>
2026-01-30 11:48:16 +0100

    first string example

---
+++ basic string.py
@@ -0,0 +1,13 @@
+text = "Hello World"
+print(text)
+text = "Hello World 2" #single vs double
+print(text[4])
+print(len(text)) # length of text
+text = ""
+print(len(text))
+
+text= "bob"
+print(text[-1]) # last character in the string
+print(text[-3])
+print(text[3])
+print(text[6/3])
